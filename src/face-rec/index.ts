fce2018443ccb3d8cdc95d21485d4e7712797e70
VikashChandraMishra <crimsonsyrus000@gmail.com>
2025-01-05 17:21:08 +0530

    added firebase

--- src/face-rec/index.ts
+++ src/face-rec/index.ts
@@ -4,28 +4,28 @@ const MODEL_URL = '/weights';
 
 const labels = ['person1Images', 'person2Images'];
 
-const labeledFaceDescriptors = await Promise.all(
-    labels.map(async label => {
-        await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
-        await faceapi.loadFaceLandmarkModel(MODEL_URL);
-        await faceapi.loadFaceRecognitionModel(MODEL_URL);
-
-        let imgs;
-        if (localStorage.getItem(label)) {
-            imgs = JSON.parse(localStorage.getItem(label) || '');
-            const img = await faceapi.fetchImage(imgs[0]);
-            const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-
-            if (!fullFaceDescription) {
-                throw new Error(`no faces detected for ${label}`);
-            }
-            const faceDescriptors = [fullFaceDescription.descriptor];
-            return new faceapi.LabeledFaceDescriptors(label, faceDescriptors);
-        }
-    })
-);
-
 export const match = async (img: any) => {
+    const labeledFaceDescriptors = await Promise.all(
+        labels.map(async label => {
+            await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+            await faceapi.loadFaceLandmarkModel(MODEL_URL);
+            await faceapi.loadFaceRecognitionModel(MODEL_URL);
+
+            let imgs;
+            if (localStorage.getItem(label)) {
+                imgs = JSON.parse(localStorage.getItem(label) || '');
+                const img = await faceapi.fetchImage(imgs[0]);
+                const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+
+                if (!fullFaceDescription) {
+                    throw new Error(`no faces detected for ${label}`);
+                }
+                const faceDescriptors = [fullFaceDescription.descriptor];
+                return new faceapi.LabeledFaceDescriptors(label, faceDescriptors);
+            }
+        })
+    );
+
     const im = await faceapi.fetchImage(img);
     let fullFaceDescriptions = await faceapi.detectAllFaces(im).withFaceLandmarks().withFaceDescriptors();
     const maxDescriptorDistance = 0.6;
